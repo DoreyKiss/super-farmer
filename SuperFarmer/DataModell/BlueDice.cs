f3f519e690b475f9cd6982d24d2b5ca2c38b0ba3
DoreyKiss <68029587+DoreyKiss@users.noreply.github.com>
2021-02-21 08:48:27 +0100

    Update SuperFarmer/DataModell/BlueDice.cs
    Co-authored-by: Metzing Márton <marton.metzing@gmail.com>

--- SuperFarmer/DataModell/BlueDice.cs
+++ SuperFarmer/DataModell/BlueDice.cs
@@ -5,12 +5,7 @@ namespace SuperFarmer.DataModell
 {
     public class BlueDice: AbstractDice
     {
-        public override List<AnimalEnum> DiceValues
-        {
-            get {return _diceValues ;}
-        }
-
-        private readonly List<AnimalEnum> _diceValues = new List<AnimalEnum>()
+        public override List<AnimalEnum> DiceValues { get; } = new List<AnimalEnum>()
                 {AnimalEnum.Fox, AnimalEnum.Pig, AnimalEnum.Sheep,
                 AnimalEnum.Bunny, AnimalEnum.Bunny, AnimalEnum.Bunny,
                 AnimalEnum.Horse, AnimalEnum.Pig, AnimalEnum.Sheep,
